12d92f863152e0a4085313620967d9840ea2900c
Fabian Döz <fabian@doez.info>
2024-07-18 15:03:10 +0200

    add Method GetMessage and deleteMessage

--- src/main/java/org/example/_02springweb_/GetMessage.java
+++ src/main/java/org/example/_02springweb_/GetMessage.java
@@ -1,9 +1,7 @@
 package org.example._02springweb_;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +13,28 @@ public class GetMessage {
     List<Message> messages = new ArrayList<>();
 
     @PostMapping("/message")
-    public Message addMessage(Message message) {
+    public Message addMessage(@RequestBody Message message) {
         messages.add(message);
-        System.out.println(message.message);
         return message;
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    @DeleteMapping("/message/{id}")
+    public Message deleteMessage(@PathVariable String id) {
+        for (Message message : messages) {
+            if (message.id().equals(id)) {
+                messages.remove(message);
+                System.out.println("Message removed");
+                return message;
+            } else {
+                System.out.println("Message not found");
+            }
+        }
+        return null;
     }
 
 }
